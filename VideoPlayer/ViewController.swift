5948fe4d3d0cacc8b6e812c5082adfd274f1e978
Sudhanshu <arpansana@IDCUBE.local>
2020-10-06 15:43:58 +0530

    initial xcode project creation

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Arpan Sana on 06/10/20.
+//  Copyright © 2020 IDCUBE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
